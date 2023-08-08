55680db277f465b902210a7af3ec597c41c3433f
Prasad Mullamuri <119662453+prasadmull@users.noreply.github.com>
2023-08-08 15:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(sampleObject.hasKey(key)){
-		return true;
+	for(let k in sampleObject){
+		if(key===k){
+			return true;
+		}
 	}
-	else{
-		return false;
-	}
+	return false;
 }
 
 // Do not change the code below
